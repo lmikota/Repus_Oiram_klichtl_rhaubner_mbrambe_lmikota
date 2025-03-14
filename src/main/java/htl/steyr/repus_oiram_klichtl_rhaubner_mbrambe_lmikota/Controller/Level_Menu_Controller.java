01c8b8b7831fede92387ddaf115bb55d83572970
Kevin Lichtl <lichtl.kevin@gmail.com>
2025-03-14 23:22:19 +0100

    aborted the idea of the tutorial-screen, instead placed the controls in an TitledPane; also added a level-menu to display all levels (not finished)

--- src/main/java/htl/steyr/repus_oiram_klichtl_rhaubner_mbrambe_lmikota/Controller/Level_Menu_Controller.java
+++ src/main/java/htl/steyr/repus_oiram_klichtl_rhaubner_mbrambe_lmikota/Controller/Level_Menu_Controller.java
@@ -7,22 +7,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Start_Menu_Controller {
+public class Level_Menu_Controller {
 
     @FXML
-    public Button startButton;
+    public Button returnButton;
 
     @FXML
-    public void onStartButtonClicked(ActionEvent actionEvent) {
+    public void onReturnButtonClicked(ActionEvent actionEvent) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/htl/steyr/repus_oiram_klichtl_rhaubner_mbrambe_lmikota/FXML-Files/level_menu-view.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/htl/steyr/repus_oiram_klichtl_rhaubner_mbrambe_lmikota/FXML-Files/start_menu-view.fxml"));
             Parent newRoot = loader.load();
 
-            Scene scene = startButton.getScene();
+            Scene scene = returnButton.getScene();
             scene.setRoot(newRoot);
 
         } catch (IOException e) {
@@ -30,8 +30,5 @@ public class Start_Menu_Controller {
         }
     }
 
-    @FXML
-    public void onExitButtonClicked(ActionEvent actionEvent) {
-        Platform.exit();
-    }
+
 }
