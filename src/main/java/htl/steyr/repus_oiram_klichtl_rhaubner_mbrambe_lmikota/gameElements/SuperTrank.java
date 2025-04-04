1f3a203742a533b05336a88dc3d420a8845a8f6b
Marcel Bramberger <brambergermarcel0@gmail.com>
2025-04-04 10:12:15 +0200

    SuperTrank is now implemented Effect: The current player gets one more life

--- src/main/java/htl/steyr/repus_oiram_klichtl_rhaubner_mbrambe_lmikota/gameElements/SuperTrank.java
+++ src/main/java/htl/steyr/repus_oiram_klichtl_rhaubner_mbrambe_lmikota/gameElements/SuperTrank.java
@@ -1,10 +1,43 @@
 package htl.steyr.repus_oiram_klichtl_rhaubner_mbrambe_lmikota.gameElements;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.awt.*;
+
 public class SuperTrank extends Item {
-    /// @ToDo
-    /// ImageFile Path sit nu ned optimal - anderes Image benutzen
-    public SuperTrank() {
-        super("Super-Trank", -1,
+    private ImageView supertrank;
+
+    public SuperTrank(Player player, Image image, double tranksize) {
+        super("Super-Trank",
                 "/htl/steyr/repus_oiram_klichtl_rhaubner_mbrambe_lmikota/img/superTrank.png");
+
+        setImagetrank(new ImageView(image));
+        activateSuperTrank(player);
+        setImagesize(tranksize);
+    }
+
+    public void activateSuperTrank(Player player) {
+        player.setHp(player.getHp() + 1);
+    }
+
+    public ImageView getSupertrank() {
+        return supertrank;
+    }
+
+    public void setImagetrank(ImageView supertrank) {
+        this.supertrank = supertrank;
+    }
+
+    public ImageView getImagetrank() {
+        return supertrank;
+    }
+
+    public void setImagesize(double tranksize){
+        getImagetrank().setFitWidth(tranksize);
+        getImagetrank().setFitHeight(tranksize);
+        getSupertrank().minHeight(tranksize);
+        getImagetrank().minWidth(tranksize);
+        getSupertrank().maxHeight(tranksize);
+        getSupertrank().maxWidth(tranksize);
     }
 }
