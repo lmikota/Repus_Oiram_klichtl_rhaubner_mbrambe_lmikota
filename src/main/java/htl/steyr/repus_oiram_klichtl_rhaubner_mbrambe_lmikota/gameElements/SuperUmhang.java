5a11de9de164dff8a4aa4ae46e05c46199b341de
Marcel Bramberger <brambergermarcel0@gmail.com>
2025-04-04 11:02:14 +0200

    SuperCape is now implemented (effect not usable jet)

--- src/main/java/htl/steyr/repus_oiram_klichtl_rhaubner_mbrambe_lmikota/gameElements/SuperUmhang.java
+++ src/main/java/htl/steyr/repus_oiram_klichtl_rhaubner_mbrambe_lmikota/gameElements/SuperUmhang.java
@@ -1,7 +1,36 @@
 package htl.steyr.repus_oiram_klichtl_rhaubner_mbrambe_lmikota.gameElements;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class SuperUmhang extends Item{
-    public SuperUmhang() {
-        super("Super-Umhang", "/");
+    private ImageView supercape;
+
+    public SuperUmhang(Image imagecape, double capesize) {
+        super("Super-Umhang", "/htl/steyr/repus_oiram_klichtl_rhaubner_mbrambe_lmikota/img/SuperCape.png");
+
+        setImagecape(new ImageView(imagecape));
+        setImagesize(capesize);
+    }
+
+    public void activateSuperCape() {
+
+    }
+
+    public ImageView getImagecape() {
+        return supercape;
+    }
+
+    public void setImagecape(ImageView supertrank) {
+        this.supercape = supertrank;
+    }
+
+    public void setImagesize(double tranksize){
+        getImagecape().setFitWidth(tranksize);
+        getImagecape().setFitHeight(tranksize);
+        getImagecape().minHeight(tranksize);
+        getImagecape().minWidth(tranksize);
+        getImagecape().maxHeight(tranksize);
+        getImagecape().maxWidth(tranksize);
     }
 }
