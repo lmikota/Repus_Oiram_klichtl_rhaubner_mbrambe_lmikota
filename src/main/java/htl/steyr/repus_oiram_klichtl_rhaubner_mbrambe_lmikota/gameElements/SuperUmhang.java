c94ded2658e6846c273f42886c8ef627fce1ba6b
Marcel Bramberger <brambergermarcel0@gmail.com>
2025-04-04 10:24:06 +0200

    inefficient code removed

--- src/main/java/htl/steyr/repus_oiram_klichtl_rhaubner_mbrambe_lmikota/gameElements/SuperUmhang.java
+++ src/main/java/htl/steyr/repus_oiram_klichtl_rhaubner_mbrambe_lmikota/gameElements/SuperUmhang.java
@@ -2,6 +2,6 @@ package htl.steyr.repus_oiram_klichtl_rhaubner_mbrambe_lmikota.gameElements;
 
 public class SuperUmhang extends Item{
     public SuperUmhang() {
-        super("Super-Umhang", -2, "/");
+        super("Super-Umhang", "/");
     }
 }
