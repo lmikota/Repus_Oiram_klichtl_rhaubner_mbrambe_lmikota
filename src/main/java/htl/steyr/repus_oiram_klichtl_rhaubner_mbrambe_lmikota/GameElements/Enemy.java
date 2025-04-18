ba11506efea3e64b96acad2ecef16fd40518b093
Robin Haubner <robinhaubner1@gmail.com>
2025-04-18 19:58:38 +0200

    now there is a tolerance for the chacking of the enemy hitiboxi if the player jumps on the goofy golem.

--- src/main/java/htl/steyr/repus_oiram_klichtl_rhaubner_mbrambe_lmikota/GameElements/Enemy.java
+++ src/main/java/htl/steyr/repus_oiram_klichtl_rhaubner_mbrambe_lmikota/GameElements/Enemy.java
@@ -39,7 +39,7 @@ public class Enemy{
                 if(playerX == enemyX+j && playerY == enemyY+i){
                     player.setPlayerVelY(0);
 
-                    if(playerY < enemyY){ //weiß noch nicht ob > oder < :-D
+                    if(playerY <= enemyY + 5){ //weiß noch nicht ob > oder < :-D
                         String enemyDeathSoundPath = "/htl/steyr/repus_oiram_klichtl_rhaubner_mbrambe_lmikota/WAV/enemyDeathSound.wav";
                         MusicPlayer.getInstance().playMusic(enemyDeathSoundPath);
 
